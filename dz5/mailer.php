539acdb915cb4942e8ad8a4abf79e93829a59b15
Roman Ivannikov <roman.y.ivannikov@gmail.com>
2020-06-19 11:09:52 +0700

    Вынес настройки в ДЗ №5 в отдельный файл

--- dz5/mailer.php
+++ dz5/mailer.php
@@ -1,18 +1,23 @@
 <?php
 require_once 'vendor/autoload.php';
+require_once 'config.php';
 
-$transport = (new Swift_SmtpTransport('smtp.yandex.ru', 465, 'ssl'))
-    ->setUsername('r.hammerok')
-    ->setPassword('pEaLNvEKVZ8aqozMAmqL')
+$transport = (new Swift_SmtpTransport(SMTP_HOST, SMTP_PORT, SMTP_ENCRYPTION))
+    ->setUsername(SMTP_USERNAME)
+    ->setPassword(SMTP_PASSWORD)
 ;
 
 $mailer = new Swift_Mailer($transport);
 
 $message = (new Swift_Message('Loft School Work #5'))
     ->setFrom(['r.hammerok@yandex.ru' => 'Roman'])
-    ->setTo(['roman.y.ivannikov@gmail.com', 'ivannikov.roman@mail.ru' => 'Roman'])
+    ->setTo(['ivannikov.roman@mail.ru' => 'Roman'])
     ->setBody('I could do it')
 ;
 
 // Send the message
-echo $result = $mailer->send($message);
+if ($mailer->send($message)) {
+    echo 'Email sent successfully';
+} else {
+    echo 'Failed to send email';
+}
